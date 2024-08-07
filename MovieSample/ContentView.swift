c8a2bba52e8d8c73d0255f6e73789288c4740c70
@specktro <specktro@nonull.mx>
2024-08-07 17:47:16 -0600

    Initial commit with gitignore file from gitignore.io

---
+++ MovieSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieSample
+//
+//  Created by Miguel Angel Gómez Rivero on 07/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieSample/MovieSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSampleApp.swift
+//  MovieSample
+//
+//  Created by Miguel Angel Gómez Rivero on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
