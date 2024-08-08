f1aa543eb293e8b2a44eb9f1055eb60ddafa2aff
@specktro <specktro@nonull.mx>
2024-08-07 18:00:46 -0600

    feat: Add movie resources and create initial card structure

--- MovieSample/ContentView.swift
+++ MovieSample/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Image("wolverine_deadpool")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Release Date: 2024-07-24")
+                        .font(.headline)
+                        .foregroundStyle(.secondary)
+                    
+                    Text("Deadpool & Wolverine")
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundStyle(.primary)
+                        .lineLimit(3)
+                    
+                    Text("Rated B")
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                }
+                
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+        .cornerRadius(10.0)
+        .overlay {
+            RoundedRectangle(cornerRadius: 10.0)
+                .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1.0)
+        }
+        .padding([.top, .horizontal])
     }
 }
 
